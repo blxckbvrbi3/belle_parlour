3973dd1a9e9d94b7cb2c47bc11e16631e6209f2c
blxckbvrbi3 <onilchib@gmail.com>
2025-03-06 03:47:12 +0200

    initial commit.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const whatsappBtn = document.querySelector(".btn-whatsapp");
+    whatsappBtn.addEventListener("click", function () {
+        alert("Redirecting to WhatsApp for booking...");
+    });
+});
